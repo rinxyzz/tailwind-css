5e39b8cf4213288ded90a553f7977b078f2092b9
RinForever <107134739+rinxyzz@users.noreply.github.com>
2023-04-10 10:43:12 +0700

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Rin Blogs And Project',
+  author: 'Riinn',
+  headerTitle: 'Rin Workspace',
+  description: 'Project Blog And Rin Workspace',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -10,8 +10,8 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'itzmerinz.dc@gmail.com',
+  github: 'https://github.com/rinxyzz',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
